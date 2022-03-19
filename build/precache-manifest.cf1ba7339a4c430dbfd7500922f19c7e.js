732e9f9a32e0673c04195ca770c120e1f40d1c52
bh2402 <70758176+bh2402@users.noreply.github.com>
2022-03-19 22:12:24 +0530

    Add files via upload

---
+++ build/precache-manifest.cf1ba7339a4c430dbfd7500922f19c7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f769841b5b2906b3d7a32e8f54740b8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f8106b355cdb733e848",
+    "url": "/static/js/2.b198b1a8.chunk.js"
+  },
+  {
+    "revision": "8ae5a798c9915ecaf237",
+    "url": "/static/js/main.4edad952.chunk.js"
+  },
+  {
+    "revision": "31cb9d8849daaa195195",
+    "url": "/static/js/runtime-main.028bd04c.js"
+  }
+]);
